c09a189656910390971d5aa17a46eac250b64d1c
SGhzal <sghzal@gmail.com>
2013-04-21 19:57:47 +0200

    position getters

--- lib/GoogleURL/GooglePosition.php
+++ lib/GoogleURL/GooglePosition.php
@@ -76,6 +76,64 @@ class GooglePosition {
 
     
     
+    public function getKeyword() {
+        return $this->keyword;
+    }
+
+    public function setKeyword($keyword) {
+        $this->keyword = $keyword;
+    }
+
+    public function getWebsite() {
+        return $this->website;
+    }
+
+    public function setWebsite($website) {
+        $this->website = $website;
+    }
+
+    public function getPosition() {
+        return $this->position;
+    }
+
+    public function setPosition($position) {
+        $this->position = $position;
+    }
+
+    public function getUrl() {
+        return $this->url;
+    }
+
+    public function setUrl($url) {
+        $this->url = $url;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    public function getSnippet() {
+        return $this->snippet;
+    }
+
+    public function setSnippet($snippet) {
+        $this->snippet = $snippet;
+    }
+
+    public function getDate() {
+        return $this->date;
+    }
+
+    public function setDate($date) {
+        $this->date = $date;
+    }
+
+
+    
     
 }
 
