95b5eb4264642f7218543083e89c5fcea8a51653
sghzal <sghzal@gmail.com>
2015-02-26 08:48:16 +0100

    In the news results

--- tests/CoreTest/NetTest.php
+++ tests/CoreTest/NetTest.php
@@ -2,15 +2,47 @@
 
 
 class NetTest  extends PHPUnit_Framework_TestCase{
-    
-   
-    
+
+
+    /**
+     * @group bla
+     */
     public function testQuery(){
+
+
+        $googleUrl = new GoogleUrl();
+        $dom = $googleUrl->search("charlie habdo");
+
+        $parser = new \GoogleUrl\Parser\NaturalParser();
+        $parser->addRule(new \GoogleUrl\Parser\Rule\ClassicalResultRule());
+        $parser->addRule(new \GoogleUrl\Parser\Rule\ClassicalResultGroupRule());
+        $parser->addRule(new \GoogleUrl\Parser\Rule\InTheNewsRule());
+
+        $parsed = $parser->parse($dom);
+
+        echo $dom->getUrl();
+
+        echo PHP_EOL . PHP_EOL;
+
+        foreach($parsed as $item){
+
+            if($item->getType() == "classical"){
+                echo $item->getPosition() . " " . $item->getTitle();
+            }else{
+                echo $item->getPosition() . " [IN THE NEWS]";
+                foreach($item->getItems() as $card){
+                    echo PHP_EOL . "   " . $card->getPosition() . " " . $card->getTitle();
+                }
+            }
+            echo PHP_EOL;
+        }
+
+        return;
         
         $googleUrl=new \GoogleUrl\DelayedQuery();
         
         $firstSearch = $googleUrl->setLang('fr')->setNumberResults(10)->search("simpson tshirt");
-        
+
         $adwords = $firstSearch->getAdwords();        
         
         $this->assertTrue(is_array($adwords->getBodyResults()));
