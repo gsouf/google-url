80b2f3e854ea57d8d0645033a95fbbb90b4cadf0
sghzal <sghzal@gmail.com>
2015-02-27 14:29:52 +0100

    Test

--- tests/CoreTest/NetTest.php
+++ tests/CoreTest/NetTest.php
@@ -11,7 +11,8 @@ class NetTest  extends PHPUnit_Framework_TestCase{
 
 
         $googleUrl = new GoogleUrl();
-        $dom = $googleUrl->search("charlie habdo");
+        $googleUrl->setLang("fr")->setTld("com.au");
+        $dom = $googleUrl->search("charlie hebdo");
 
         $parser = new \GoogleUrl\Parser\NaturalParser();
         $parser->addRule(new \GoogleUrl\Parser\Rule\ClassicalResultRule());
@@ -26,35 +27,35 @@ class NetTest  extends PHPUnit_Framework_TestCase{
 
         foreach($parsed as $item){
 
-            if($item->getType() == "classical"){
-                echo $item->getPosition() . " " . $item->getTitle();
-            }else{
-                echo $item->getPosition() . " [IN THE NEWS]";
+            if($item->type == "classical"){
+                echo $item->position . " " . $item->title;
+            }else if($item->type == "inTheNews" ){
+                echo $item->position . " [IN THE NEWS]";
                 foreach($item->getItems() as $card){
-                    echo PHP_EOL . "   " . $card->getPosition() . " " . $card->getTitle();
+                    echo PHP_EOL . "   " . $card->position . " " . $card->title;
                 }
             }
             echo PHP_EOL;
         }
 
         return;
-        
+
         $googleUrl=new \GoogleUrl\DelayedQuery();
-        
+
         $firstSearch = $googleUrl->setLang('fr')->setNumberResults(10)->search("simpson tshirt");
 
-        $adwords = $firstSearch->getAdwords();        
-        
+        $adwords = $firstSearch->getAdwords();
+
         $this->assertTrue(is_array($adwords->getBodyResults()));
         $this->assertTrue(is_array($adwords->getColumnResults()));
-        
+
         $this->assertTrue(count($adwords)>0);
         $this->assertEquals(10, count($firstSearch->getPositions()));
         $this->assertEquals(10, count($googleUrl->setLang('fr')->setNumberResults(10)->setPage(2)->search("acdc")->getPositions()));
 
         $this->assertEquals(true,  GoogleUrl::langageIsAvailable("fr"));
         $this->assertEquals(true,  GoogleUrl::langageIsAvailable("en"));
-        
+
         $this->assertEquals(false,  GoogleUrl::langageIsAvailable("rf"));
         
     }
