41f3996753378baa80be2379312d7286fc51bb0f
sghzal <sghzal@gmail.com>
2015-10-11 15:01:04 +0200

    update CI

---
+++ test/bin/ci.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+SCRIPTFILE=$(readlink -f "$0")
+SCRIPTDIR=$(dirname "$SCRIPTFILE")
+
+phpunit -c "$SCRIPTDIR/../../phpunit.dist.xml" --coverage-clover "$SCRIPTDIR/../../build/logs/clover.xml"
+
+#$SCRIPTDIR/phpcs.bash $1
+
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]
+then
+    ./vendor/bin/test-reporter
+fi
