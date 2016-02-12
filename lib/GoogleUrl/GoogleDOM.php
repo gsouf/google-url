8a3634368237d9d7fac51c340ebdd7581b8f99c4
sghzal <sghzal@gmail.com>
2016-02-12 11:04:11 +0100

    fix search results

--- lib/GoogleUrl/GoogleDOM.php
+++ lib/GoogleUrl/GoogleDOM.php
@@ -18,7 +18,7 @@ class GoogleDOM extends \DOMDocument{
     /**
      * list of natural nodes
      */
-    const NATURAL_QUERY="//div[@id = 'ires']/ol/descendant::*[self::div or self::li][@class='g']";
+    const NATURAL_QUERY="//div[@id = 'ires']/div[@id = 'rso']/descendant::*[self::div or self::li][@class='g']";
     /**
      * Get natural link (<a> tag) in the natural node context
      */
